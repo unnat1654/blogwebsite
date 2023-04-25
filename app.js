fd54d5e8bdb11886d986c0855d13717c1f4039e0
Unnat1654 <125393204+unnat1654@users.noreply.github.com>
2023-04-25 15:38:20 +0530

    added signup and login api

--- app.js
+++ app.js
@@ -13,8 +13,12 @@ app.use(express.static("public"));
 const ejs = require("ejs");
 app.set('view engine', 'ejs');
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
-const dbcall = require(__dirname+"/mongocall.js");
+
+
+const dbcall = require(__dirname + "/mongocall.js");
 
 const _ = require("lodash");
 app.get("/", (req, res) => {
@@ -23,18 +27,64 @@ app.get("/", (req, res) => {
 app.get("/home", (req, res) => {
   res.render("home");
 });
-app.get("/compose", (req,res)=> {
+app.get("/compose", (req, res) => {
   res.render("compose");
 });
-app.get("/:pagename",async (req,res)=> {
-  const pagetitle = _.lowerCase(req.params.pagename);
-  const mongocall = await dbcall.mongocall();
-  
-  mongocall.find({title: pagetitle}).toArray().then((blogcontentarray) => {res.render("post",{blogTitle: _.startCase(pagetitle), blogContent: blogcontentarray[0].content});});
-  
+app.get("/signup", (req, res) => {
+  res.render("signup", { username_taken: "userEmailNotTaken" });
 });
-app.post("/compose", async (req,res)=> {
-  const mongocall = await dbcall.mongocall();
+app.get("/login", (req, res) => {
+  res.render("login", {username_check: "user-exists",password_check: "password-right" });
+});
+app.get("/:pagename", async (req, res) => {
+  const pagetitle = _.lowerCase(req.params.pagename);
+  const mongocall = await dbcall.mongocall("titlencontent");
+  mongocall.find({ title: pagetitle }).toArray().then((blogcontentarray) => { res.render("post", { blogTitle: blogcontentarray[0]?.title, blogContent: blogcontentarray[0]?.content }); });
+});
+app.post("/signup", async (req, res) => {
+  const users_and_their_data = await dbcall.mongocall("users_and_their_data");
+  const user_table = await users_and_their_data.findOne({ username: req.body.username });
+  const mail_table = await users_and_their_data.findOne({ email: req.body.email });
+  if (user_table || mail_table) {
+    res.render("signup", { username_taken: "userEmailTaken" });
+  }
+  else {
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+      if (err) { console.log(err); }
+      users_and_their_data.insertOne({
+        "username": req.body.username,
+        "email": req.body.email,
+        "password": hash
+      });
+    });
+  }
+});
+app.post("/login", async (req, res) => {
+  const users_and_their_data = await dbcall.mongocall("users_and_their_data");
+  const user_table = await users_and_their_data.findOne({ username: req.body.username });
+  if (user_table) {
+    bcrypt.compare(req.body.password, user_table.password, (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      //in case password is true
+      else if (result == true) {
+        res.redirect("/home");
+      }
+      //in case password is false
+      else if (result == false) {
+        console.log(req.body.password);
+        res.render("login", {username_check: "user-exists",password_check: "password-wrong" });
+      }
+
+    });
+  }
+  else{
+    res.render("login", {username_check: "user-not-exists",password_check: "password-right" });
+  }
+});
+app.post("/compose", async (req, res) => {
+  const mongocall = await dbcall.mongocall("titlencontent");
   const pagetitle = _.lowerCase(req.body.postTitle);
   mongocall.insertOne({
     title: pagetitle,
@@ -42,7 +92,10 @@ app.post("/compose", async (req,res)=> {
   });
   res.redirect("/");
 });
-app.post("/like",async (req,res) => {
+app.post("/like", async (req, res) => {
+  console.log(req.body);
+});
+app.post("/unlike", async (req, res) => {
   console.log(req.body);
 });
 
--- mongocall.js
+++ mongocall.js
@@ -2,9 +2,9 @@ const mongodb = require("mongodb");
 const url= "mongodb+srv://devolopkingbro:XRO1k8cSHFJPj8bM@cluster0.gresqe3.mongodb.net/test";
 const client = new mongodb.MongoClient(url);
 
-const mongocall = async (req,res)=> {
+const mongocall = async (collection)=> {
     const result = await client.connect();
     const db = result.db("blogweb");
-    return db.collection("titlencontent");
+    return db.collection(collection);
 }
 module.exports.mongocall = mongocall;
